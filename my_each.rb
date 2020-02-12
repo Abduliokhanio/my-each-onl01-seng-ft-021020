b3f83a81f961967aa785e253ef63c1a2d50f2e76
Abdul Khan <akhan17@outlook.com>
2020-02-12 20:58:34 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each do|i|
-  puts i
+collection = [1, 2, 3, 4]
+array2 = []
+def my_each(array)
+  array2 = []
+  count = 0
+  while count != array.length
+    array2.push(array[count])
+    count +=1
+  end
+  yield(array2)
 end
+
+
+my_each(collection){puts collection}
